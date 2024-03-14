771b1be946f3813c500de47990b279dca5aa643e
Kamshory, MT <kamshory@yahoo.com>
2024-03-14 16:21:06 +0700

    Initial commits

---
+++ WebSocket/Applications/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WS\Applications;
+
+use MagicObject\SetterGetter;
+
+class Config extends SetterGetter
+{
+    
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '772030c331787cdbae370d23e49b6aac0dd93a5b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '772030c331787cdbae370d23e49b6aac0dd93a5b',
+            'dev_requirement' => false,
+        ),
+        'planetbiru/magic-object' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../planetbiru/magic-object',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '7de65b2244c177a77783de1894170a8929c29032',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'pretty_version' => 'v1.29.0',
+            'version' => '1.29.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
+            'aliases' => array(),
+            'reference' => 'ef4d7e442ca910c4764bce785146269b30cb5fc4',
+            'dev_requirement' => false,
+        ),
+        'symfony/yaml' => array(
+            'pretty_version' => 'v3.4.47',
+            'version' => '3.4.47.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/yaml',
+            'aliases' => array(),
+            'reference' => '88289caa3c166321883f67fe5130188ebbb47094',
+            'dev_requirement' => false,
+        ),
+    ),
+);
